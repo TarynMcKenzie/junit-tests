f813892d47fa4a9d49c841094682590d4a65b429
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-04-09 16:36:14 -0500

    File setup complete for second part to exercise

--- src/test/java/CohortTest.java
+++ src/test/java/CohortTest.java
@@ -5,6 +5,16 @@ import java.util.ArrayList;
 import java.util.List;
 import static org.junit.Assert.*;
 
+
+// TODO: Once you are done with the Student.java class.
+//          1. Checkout to the cohorts-feature branch, there you will find a Cohort.java class inside the src/main/java folder, this class has already a lot of methods to calculate the cohort average grade and add a student. Let's go ahead and make sure there's sufficient tests for this class to be deployed to production:
+//          2. Start by creating a new branch called: cohorts-tests.
+//          3. Then create a CohortTest class and build a test for each of the following requirements:
+//                  a. A Cohort instance can add a Student to the List of students.
+//                  b. A Cohort instance can get the current List of students.
+//                  c. A Cohort instance can get the average and it's being calculated correctly.
+//          4. Go back to the StudentTest class and refactor the code to avoid any repetition in the test cases, the use of the @Before annotation will be useful to achieve this, do the same with this new CohortTest class if you find any code repetition as well.
+
 public class CohortTest {
 
     Cohort emptyCohort;
